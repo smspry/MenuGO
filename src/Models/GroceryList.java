b2329ac6f6e2eb862478abdc355defb1ed372f4f
smspry <smspry@wpi.edu>
2020-08-15 16:44:10 -0400

    added meal item and grocery list classes round 2

---
+++ src/Models/GroceryList.java
@@ -0,0 +1,23 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    ArrayList<MealItem> mealItems;
+
+    public GroceryList(ArrayList<MealItem> mealItems) {
+        this.mealItems = mealItems;
+    }
+
+    public ArrayList<MealItem> getMealItems() {
+        return mealItems;
+    }
+
+    public void setMealItems(ArrayList<MealItem> mealItems) {
+        this.mealItems = mealItems;
+    }
+
+    public void exportList(Menu menu) {
+
+    }
+}
---
+++ src/Models/MealItem.java
@@ -0,0 +1,27 @@
+package Models;
+
+public class MealItem {
+    private Ingredient ingredient;
+    private Quantity quantity;
+
+    public MealItem(Ingredient ingredient, Quantity quantity) {
+        this.ingredient = ingredient;
+        this.quantity = quantity;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Quantity getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Quantity quantity) {
+        this.quantity = quantity;
+    }
+}
