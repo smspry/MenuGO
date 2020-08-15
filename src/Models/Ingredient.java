bc9b610e8d7ea7b974186cbac6c929e2ec6f2810
pjmara <pjmarawork@gmail.com>
2020-08-15 16:14:42 -0400

    added modules folder, filled some in

---
+++ src/Models/Ingredient.java
@@ -0,0 +1,28 @@
+package Models;
+
+public class Ingredient {
+
+    private String name;
+    private String category;
+
+    public Ingredient(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
