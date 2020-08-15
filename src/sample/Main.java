f953b76ce28903b5f066ac59c3b0f1bc9d414df1
pjmara <pjmarawork@gmail.com>
2020-08-15 02:22:27 -0400

    changed to bedtime

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Selina");
+        primaryStage.setTitle("Hello Bedtime");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
