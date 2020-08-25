d2936586720dae0363745b497fe2e9f13acdab22
smspry <smspry@wpi.edu>
2020-08-25 11:41:01 -0400

    nice create meal views

--- src/Controllers/CreateMealController.java
+++ src/Controllers/CreateMealController.java
@@ -11,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
@@ -19,6 +20,7 @@ import javafx.stage.Stage;
 
 //import javax.swing.text.html.TableView;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class CreateMealController extends ViewsController{
@@ -29,7 +31,7 @@ public class CreateMealController extends ViewsController{
     private GridPane grid;
 
     @FXML
-    private Button home, addIngredient, addMeal, done;
+    private Button home, addIngredient, addMeal, save;
 
     @FXML
     private void buttonPressedEvent(ActionEvent event) throws IOException {
@@ -58,9 +60,9 @@ public class CreateMealController extends ViewsController{
             addRowCounter = 0;
             ViewsController.newScene(stage, root);
         }
-        else if (event.getSource() == done){
+        else if (event.getSource() == save){
             addRowCounter = 0;
-            stage = (Stage) done.getScene().getWindow();
+            stage = (Stage) save.getScene().getWindow();
             loader.setLocation(getClass().getResource("/sample/ListOfMeals.fxml"));
             root = loader.load();
             ViewsController.newScene(stage, root);
@@ -68,16 +70,20 @@ public class CreateMealController extends ViewsController{
     }
 
     private GridPane addInputRow(int addRowCounter) {
+        // creating children in for the new row
         TextField ingredient= new TextField();
         ingredient.setPromptText("Chicken, peppers, etc...");
+
         TextField quantity = new TextField();
         quantity.setPromptText("1, 10, 1/4, etc...");
-        ChoiceBox measurement = new ChoiceBox();
+
+        ComboBox measurement = new ComboBox<Array>();
+        measurement.setPrefWidth(150.00);
         QuantityAccess na = new QuantityAccess();
         ArrayList<String> items = na.getAllQuantities();
         measurement.setItems(FXCollections.observableList(items));
-        measurement.prefWidth(150.00);
 
+        // setting up additional row
         RowConstraints newRow = new RowConstraints();
         newRow.setPrefHeight(40);
         newRow.setValignment(VPos.CENTER);
